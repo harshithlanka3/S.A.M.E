397c866b73c374532ebedfec7e7c1d9a94882abf
harshithlanka3 <harshithlanka03@gmail.com>
2023-11-25 15:31:46 -0500

    Added dialogue box package

--- app/s_a_m_e/lib/add.dart
+++ app/s_a_m_e/lib/add.dart
@@ -11,54 +11,47 @@ class Add extends StatelessWidget {
       appBar: AppBar(
         title: const Text('S.A.M.E'),
       ),
-      
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-          const Text('Add Symptom, Chief Complaint, and Diagnosis'),
-          const Padding(padding: EdgeInsets.all(10.0)),
-          const TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder (
-                borderSide: BorderSide(
-                  color: navy,
-                  width: 2.0,
-                )
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            const Text('Add Symptom, Chief Complaint, and Diagnosis'),
+            const Padding(padding: EdgeInsets.all(10.0)),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: navy,
+                    width: 2.0,
+                  )),
+                  labelText: 'Enter Symptom',
+                  hintText: 'Enter Symptom Name'),
+            ),
+            const Padding(padding: EdgeInsets.all(8.0)),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: navy,
+                    width: 2.0,
+                  )),
+                  labelText: 'Enter Chief Complaint',
+                  hintText: 'Enter Complaint Category'),
+            ),
+            const Padding(padding: EdgeInsets.all(16.0)),
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(navy),
               ),
-              labelText: 'Enter Symptom',
-              hintText: 'Enter Symptom Name'
+              child: const Text('Add to Database'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Symptoms()),
+                );
+              },
             ),
-          ),
-          const Padding(padding: EdgeInsets.all(8.0)),
-          const TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder (
-                borderSide: BorderSide(
-                  color: navy,
-                  width: 2.0,
-                )
-              ),
-              labelText: 'Enter Chief Complaint',
-              hintText: 'Enter Complaint Category'
-            ),
-          ),
-          const Padding(padding: EdgeInsets.all(16.0)),
-          ElevatedButton(
-            style: const ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll<Color>(navy),
-            ),
-            child: const Text('Add to Database'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Symptoms()),
-              );
-            },
-          ),
-        ]
-        )
-      ),
+          ])),
     );
   }
 }
